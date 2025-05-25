9211b93f60ba8c9c56f9c60e902668a2f1d62f80
nblaanjani <nblaanjani@gmail.com>
2025-05-25 19:09:45 +0700

    Create Pemain.java

---
+++ Pemain.java
@@ -0,0 +1,22 @@
+public class Pemain {
+    private int tinggi;
+    private int berat;
+
+    public Pemain(int tinggi, int berat) {
+        this.tinggi = tinggi;
+        this.berat = berat;
+    }
+
+    public int getTinggi() {
+        return tinggi;
+    }
+
+    public int getBerat() {
+        return berat;
+    }
+
+    @Override
+    public String toString() {
+        return "(Tinggi: " + tinggi + " cm, Berat: " + berat + " kg)";
+    }
+}
